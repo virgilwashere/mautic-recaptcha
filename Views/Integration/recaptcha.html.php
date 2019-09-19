3cc8dcb17e360164f9bd318af05dfbcb4f75650e
Virgil <virgil@endless.net.au>
2019-09-19 10:43:15 +1000

    Explicitly render the reCAPTCHA v2 widget
    - use the async and defer parameters in the script tags
    - `grecaptcha.render` allows further customisations
      - <https://developers.google.com/recaptcha/docs/display#render_param>
    - Include `expired-callback` function

--- Views/Integration/recaptcha.html.php
+++ Views/Integration/recaptcha.html.php
@@ -1,16 +1,25 @@
 <?php
-
-$defaultInputClass = (isset($inputClass)) ? $inputClass : 'input';
-$containerType     = 'div-wrapper';
+/*
+ * @copyright   2019 Mautic Contributors. All rights reserved
+ *
+ * @author      Konstantin Scheumann
+ * @author      Virgil
+ *
+ * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
+ */
 
 include __DIR__.'/../../../../app/bundles/FormBundle/Views/Field/field_helper.php';
+
+$containerType     = 'div-wrapper';
+$defaultInputClass = (isset($inputClass)) ? $inputClass : 'input';
 
 $action   = $app->getRequest()->get('objectAction');
 $settings = $field['properties'];
 
-$formName    = str_replace('_', '', $formName);
+// $formName       = !empty($formName) ? $formName : '';
+$formName       = str_replace('_', '', $formName);
 $hashedFormName = md5($formName);
-$formButtons = (!empty($inForm)) ? $view->render(
+$formButtons    = (!empty($inForm)) ? $view->render(
     'MauticFormBundle:Builder:actions.html.php',
     [
         'deleted'        => false,
@@ -27,27 +36,45 @@ $label = (!$field['showLabel'])
 <label $labelAttr>{$view->escape($field['label'])}</label>
 HTML;
 
-$jsElement = <<<JSELEMENT
-	<script type="text/javascript">
-    function verifyCallback_{$hashedFormName}( response ) {
-        document.getElementById("mauticform_input_{$formName}_{$field['alias']}").value = response;
-    }
-</script>
-<script src='https://www.google.com/recaptcha/api.js'></script>
-JSELEMENT;
+$jsCallback = <<<GRECAPTCHA_CALLBACK
+    <script type="text/javascript">
+        var verifyCallback_{$hashedFormName}( response ) {
+            document.getElementById("mauticform_input_{$formName}_{$field['alias']}").value = response;
+        };
+        var expCallback = function() {
+            grecaptcha.reset();
+        };
+        var onloadCallback = function() {
+            grecaptcha.render("mauticform_{$formName}_{$field['alias']}", {
+                'sitekey' : "{$field['customParameters']['site_key']}",
+                'callback' : verifyCallback_{$hashedFormName},
+                'expired-callback': expCallback,
+                'theme' : 'dark',
+                // 'size' : 'compact',
+            });
+        };
+    </script>
+GRECAPTCHA_CALLBACK;
+
+$jsAPI = <<<RECAPTCHA_API
+    <!-- Explicitly render the reCAPTCHA widget -->
+    <script src="https://www.google.com/recaptcha/api.js?onload=onloadCallback&render=explicit"
+        async defer>
+    </script>
+RECAPTCHA_API;
 
 $html = <<<HTML
-    {$jsElement}
-	<div $containerAttr>
+    {$jsCallback}
+    <div $containerAttr>
         {$label}
-	    <div class="g-recaptcha" data-sitekey="{$field['customParameters']['site_key']}" data-callback="verifyCallback_{$hashedFormName}"></div>
+        <!-- <div class="g-recaptcha" data-sitekey="{$field['customParameters']['site_key']}" data-callback="verifyCallback_{$hashedFormName}" data-expired-callback="expCallback"></div> -->
+        <div class="g-recaptcha"></div>
         <input $inputAttr type="hidden">
         <span class="mauticform-errormsg" style="display: none;"></span>
     </div>
+    {$jsAPI}
 HTML;
 ?>
-
-
 
 <?php
 echo $html;
